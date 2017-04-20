a66825b88b682c0d892f29e3e23b908ced04364e
HONG LY <hongly@HONGs-Mac.local>
2017-04-20 13:03:26 +1000

    1st Check in

---
+++ TripGalleryApp/TripGalleryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TripGalleryApp
+//
+//  Created by HONG LY on 4/20/17.
+//  Copyright © 2017 HONG LY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
